23ab73487acba6f03a48baeacf312dcd747023c4
Catalin Afrasinei <afr.catalin@gmail.com>
2024-06-02 20:27:53 +0300

    - added flag to enable logging to file

--- config/logger.py
+++ config/logger.py
@@ -3,23 +3,25 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 
-def setup_logger():
+def setup_logger(to_file=False):
     logger = logging.getLogger('')
     logger.setLevel(logging.DEBUG)  # Set to INFO or WARNING for production
 
-    # Create file handler which logs even debug messages
-    fh = RotatingFileHandler('application.log', maxBytes=10485760, backupCount=5)
-    fh.setLevel(logging.DEBUG)  # Adjust as necessary for file handler
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # Create and add file handler if flag is set
+    if to_file:
+        fh = RotatingFileHandler('application.log', maxBytes=10485760, backupCount=5)
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
 
     # Create console handler with a higher log level
     ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)  # Adjust as necessary for console output
+    ch.setLevel(logging.INFO)
 
-    # Create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
+    # Add formatter to the handler
     ch.setFormatter(formatter)
 
     # Add the handlers to the logger
-    logger.addHandler(fh)
     logger.addHandler(ch)
